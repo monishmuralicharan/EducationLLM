eedb96e069535db19b4436853d3320b423dba35f
monish237 <monish.muralicharan@gmail.com>
2024-08-23 19:02:15 -0400

    pinecone

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ import os
 
 from groq import Groq
 
+from pinecone import Pinecone, ServerlessSpec
+
 load_dotenv()
 
 client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+pc = Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY")
+)
+
+pc.create_index(
+    name="quickstart",
+    dimension=2, # Replace with your model dimensions
+    metric="cosine", # Replace with your model metric
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    ) 
 )
 
 chat_input = input("What is your question: ")
