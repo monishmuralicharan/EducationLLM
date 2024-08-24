fc7afd74242093c680f527eee10bbf80513dd5a0
monish237 <monish.muralicharan@gmail.com>
2024-08-24 17:33:30 -0400

    streamlit + rag pipeline

---
+++ streamlitApp.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from testLLM import RAGPipeline
+
+rag_pipeline = RAGPipeline()
+
+st.title("LLM app")
+
+user_query = st.text_input("Enter your query:")
+
+if user_query:
+    with st.spinner("retrieving information..."):
+        response = rag_pipeline.run_pipeline(user_query)
+    st.write(response)
+
---
+++ testLLM.py
@@ -0,0 +1,85 @@
+from dotenv import load_dotenv
+import os
+
+from transformers import RobertaModel, RobertaTokenizer
+import torch
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from groq import Groq
+
+# loads environemnt variables
+load_dotenv()
+
+class RAGPipeline():
+    def __init__(self):
+        # initializes groq and pinecone clients 
+        self.client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+        self.pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+
+        # grabbing the index
+        self.index_name = "sample-movies"
+        self.index = self.pc.Index(self.index_name)
+
+        # initializing the embedding model
+        self.tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
+        self.model = RobertaModel.from_pretrained('roberta-large')
+
+    # function to generate embedding from input query
+    def text_to_embedding(self, text):
+        # Tokenize the input text
+        inputs = self.tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=128)
+
+        # Generate the embedding by passing the input through the RoBERTa model
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+
+        # Use the [CLS] token's output as the embedding (first token of the sequence)
+        embedding = outputs.last_hidden_state[:, 0, :].squeeze()
+
+        return embedding.numpy()
+
+    def retrieve_context(self, query_embedding, top_k=5):
+        # querying the vectorDB
+        response = self.index.query(
+            vector= query_embedding,
+            top_k=top_k,
+            include_metadata=True 
+        )
+        # extracting the context from DB response
+        context = "\n".join([
+            f"Title: {item['metadata']['title']}\n"
+            f"Genre: {item['metadata']['genre']}\n"
+            f"Box Office: {item['metadata']['box-office']}\n"
+            f"Summary: {item['metadata']['summary']}\n"
+            for item in response['matches']
+        ])
+        return context
+
+    def generate_response(self, user_query, context):
+        # calling groq model
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant providing movie recommendations."
+                },
+                {
+                    "role": "user",
+                    "content": user_query,
+                },
+                {
+                    "role":"system",
+                    "content": context,
+                }
+            ],
+            model="llama3-8b-8192",
+        )
+        return chat_completion.choices[0].message.content
+
+    def run_pipeline(self, user_query):
+        embedded_query = self.text_to_embedding(user_query)
+        context = self.retrieve_context(embedded_query)
+        response = self.generate_response(embedded_query, context)
+        return response
+
+
